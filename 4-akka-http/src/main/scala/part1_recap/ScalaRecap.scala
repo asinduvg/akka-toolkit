5c9d6cd0ebac498d82f532cccaa1358e331a32b8
Asindu <ashvg.dev@gmail.com>
2021-12-28 09:17:41 +0530

    scala recap

---
+++ 4-akka-http/src/main/scala/part1_recap/ScalaRecap.scala
@@ -0,0 +1,135 @@
+package part1_recap
+
+import scala.concurrent.Future
+import scala.language.implicitConversions
+import scala.util.{Failure, Success}
+
+object ScalaRecap extends App {
+
+  val aCondition: Boolean = false
+
+  def myFunction(x: Int) = {
+    // code
+    if (x > 4) 42 else 65
+  }
+  // instructions vs expressions
+  // types + type inference
+
+  // OO features of Scala
+  class Animal
+
+  trait Carnivore {
+    def eat(a: Animal): Unit
+  }
+
+  object Carnivore
+
+  // generics
+  abstract class MyList[+A]
+
+  // method notations
+  1 + 2 // infix notation
+  1.+(2)
+
+  // FP
+  val anIncrementer: Int => Int = (x: Int) => x + 1
+  anIncrementer(1)
+
+  List(1, 2, 3).map(anIncrementer)
+  // HOF: flatMap, filter
+  // for-comprehensions
+
+  // Monads: Options, Try
+
+  // Pattern matching!
+  val unknown: Any = 2
+  val order = unknown match {
+    case 1 => "first"
+    case 2 => "second"
+    case _ => "unknown"
+  }
+
+  try {
+    // code that can throw an exception
+    throw new RuntimeException
+  } catch {
+    case e: Exception => println("I caught one!")
+  }
+
+  /**
+   * Scala advanced
+   */
+  // multithreading
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  // ExecutionContext - manager of threads
+
+  val future = Future {
+    // long computation here
+    // executed on SOME other thread
+    42
+  }
+  // map, flatMap, filter + other niceties e.g. recover/recoverWith
+
+  future onComplete {
+    case Success(value) => println(s"I found the meaning of life: $value")
+    case Failure(ex) => println(s"I found $ex while searching for the meaning of life!")
+  } // on SOME thread
+
+  val partialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 64
+    case _ => 999
+  }
+  // based on pattern matching
+
+  // type aliases
+  type AkkaReceive = PartialFunction[Any, Unit]
+
+  def receive: AkkaReceive = {
+    case 1 => println("Hello!")
+    case _ => println("confused...")
+  }
+
+  // Implicits
+  implicit val timeout = 3000
+
+  def setTimeout(f: () => Unit)(implicit timeout: Int) = f()
+
+  setTimeout(() => println("timeout")) // other arg list inject by the compiler
+
+  // conversions
+  // 1) Implicit methods
+  case class Person(name: String) {
+    def greet: String = s"Hi, my name is $name"
+  }
+
+  implicit def fromStringToPerson(name: String): Person = Person(name)
+
+  "Person".greet
+  // fromStringToPerson("Peter").greet
+
+  // 2) Implicit classes
+  implicit class Dog(name: String) {
+    def bark = println("Bark!")
+  }
+
+  "Lassie".bark
+  // new Dog("Lassie").bark
+
+  // implicit organizations
+  // i - local scope
+  implicit val numberOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  List(1, 2, 3).sorted // (numberOrdering) => List(3, 2, 1)
+
+  // ii - imported scope
+  // iii - companion objects of the types involved in the call
+  object Person {
+    implicit val personOrdering: Ordering[Person] = Ordering
+      .fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+  }
+
+  List(Person("Bob"), Person("Alice")).sorted // (Person.personOrdering)
+  // => List(Person("Alice"), Person("Bob"))
+
+}
